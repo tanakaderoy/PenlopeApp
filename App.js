fe919c52b98ba85f714e6eea3e5f9ab671b3f3c8
Tanaka Mazivanhanga <42681052+tanakaderoy@users.noreply.github.com>
2023-04-01 01:19:25 -0400

    😂 lol idk man

--- App.js
+++ App.js
@@ -1,85 +1,110 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useCallback, useRef } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useCallback } from 'react';
+
 import {
-  Animated,
   Button,
   Image,
+  Linking,
   StyleSheet,
   Text,
   useWindowDimensions,
   View,
-} from "react-native";
+} from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 function NarcanScreen() {
-  const flipAnimation = useRef(new Animated.Value(0)).current;
+  const flip = useSharedValue(0);
+  const flipToFrontStyle = useAnimatedStyle(() => {
+    let rotation = interpolate(flip.value, [0, 180], [0, 180]);
+
+    return {
+      transform: [
+        {
+          rotateY: `${rotation}deg`,
+        },
+      ],
+    };
+  });
+  const flipToBackStyle = useAnimatedStyle(() => {
+    let rotation = interpolate(flip.value, [0, 180], [180, 360]);
+
+    return {
+      transform: [
+        {
+          rotateY: `${rotation}deg`,
+        },
+      ],
+    };
+  });
 
   const { width, height } = useWindowDimensions();
 
-  let flipRotation = 0;
+  // const flipToFrontStyle = {
+  //   transform: [
+  //     {
+  //       rotateY: flipAnimation.interpolate({
+  //         inputRange: [0, 180],
+  //         outputRange: ['0deg', '180deg'],
+  //       }),
+  //     },
+  //   ],
+  // };
 
-  flipAnimation.addListener(({ value }) => (flipRotation = value));
-
-  const flipToFrontStyle = {
-    transform: [
-      {
-        rotateY: flipAnimation.interpolate({
-          inputRange: [0, 180],
-          outputRange: ["0deg", "180deg"],
-        }),
-      },
-    ],
-  };
-
-  const flipToBackStyle = {
-    transform: [
-      {
-        rotateY: flipAnimation.interpolate({
-          inputRange: [0, 180],
-          outputRange: ["180deg", "360deg"],
-        }),
-      },
-    ],
-  };
+  // const flipToBackStyle = {
+  //   transform: [
+  //     {
+  //       rotateY: flipAnimation.interpolate({
+  //         inputRange: [0, 180],
+  //         outputRange: ['180deg', '360deg'],
+  //       }),
+  //     },
+  //   ],
+  // };
 
   const flipToFront = useCallback(() => {
-    Animated.timing(flipAnimation, {
-      toValue: 180,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
+    // Animated.timing(flipAnimation, {
+    //   toValue: 180,
+    //   duration: 300,
+    //   useNativeDriver: true,
+    // }).start();
+    flip.value = withTiming(180, { duration: 300 });
   }, []);
 
   const fliptoBack = useCallback(() => {
-    Animated.timing(flipAnimation, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
+    // Animated.timing(flipAnimation, {
+    //   toValue: 0,
+    //   duration: 300,
+    //   useNativeDriver: true,
+    // }).start();
+    flip.value = withTiming(0, { duration: 300 });
   }, []);
 
   const onPress = useCallback(() => {
-    !!flipRotation ? fliptoBack() : flipToFront();
+    !!flip.value ? fliptoBack() : flipToFront();
   }, []);
 
   return (
     <View style={styles.container}>
       <View
         style={{
-          borderColor: "red",
-          borderWidth: 1,
           marginHorizontal: 16,
         }}
       >
         <NarcanImage
           style={[
             {
-              width: width,
-              height: height / 2,
+              width: 400,
+              height: 400,
               borderRadius: 16,
-              position: "absolute",
-              justifyContent: "center",
-              alignItems: "center",
+              position: 'absolute',
+              justifyContent: 'center',
+              alignItems: 'center',
             },
             flipToBackStyle,
           ]}
@@ -87,19 +112,19 @@ function NarcanScreen() {
         <Directions
           style={[
             {
-              width,
-              height: height / 2,
-              backgroundColor: "#FF8787",
+              width: 400,
+              height: 450,
+              backgroundColor: '#FF8787',
               borderRadius: 16,
-              backfaceVisibility: "hidden",
-              alignItems: "center",
-              justifyContent: "center",
+              backfaceVisibility: 'hidden',
+              alignItems: 'center',
+              justifyContent: 'center',
             },
             flipToFrontStyle,
           ]}
         />
       </View>
-      <View style={[{ width: "100%", paddingTop: 30 }, flipToFrontStyle]}>
+      <View style={[{ width: 400, paddingTop: 30, borderRadius: 10 }]}>
         <Button title="Flip" onPress={onPress} />
       </View>
     </View>
@@ -110,29 +135,32 @@ function Directions(p) {
   const { style, ...props } = p;
   return (
     <Animated.View style={[style]} {...props}>
-      <Text style={{ fontSize: 40, fontweight: "700" }}>Directions:</Text>
+      <Text style={{ fontSize: 40, fontWeight: '700' }}>Directions:</Text>
       <View
         style={{
-          borderColor: "red",
-          borderWidth: 1,
-          width: "100%",
+          width: '100%',
+          paddingHorizontal: 16,
         }}
       >
-        Gently insert the Narcan spray into one nostril, positioning your
-        pointer and middle finger on either side of the nose leaving your thumb
-        avaliable to push the nasal spray. Using your thumb push the spray into
-        the nostril and wait 2 minutes. If the patient does not respond in 2
-        minutes open a pack and repeat the steps in the other nostril.
+        <Text>
+          Gently insert the Narcan spray into one nostril, positioning your
+          pointer and middle finger on either side of the nose leaving your
+          thumb avaliable to push the nasal spray. Using your thumb push the
+          spray into the nostril and wait 2 minutes. If the patient does not
+          respond in 2 minutes open a pack and repeat the steps in the other
+          nostril.
+        </Text>
       </View>
 
       <Overdose />
+      <Help />
     </Animated.View>
   );
 }
 function NarcanImage(p) {
   const { style, ...props } = p;
   const img = {
-    uri: "https://cdn.shopify.com/s/files/1/0996/0350/products/narcan_overdose_nasal_spray_1800x1800.jpg?v=1661946912",
+    uri: 'https://cdn.shopify.com/s/files/1/0996/0350/products/narcan_overdose_nasal_spray_1800x1800.jpg?v=1661946912',
   };
   return (
     <Animated.View style={[style]} {...props}>
@@ -142,30 +170,68 @@ function NarcanImage(p) {
 }
 function Overdose() {
   return (
-    <View style={{ width: "100%", marginTop: 40 }}>
-      <Text style={{ alignSelf: "center", fontSize: 40, fontweight: "700" }}>
-        Signs of Overdose:
+    <View style={{ width: '100%', marginTop: 40 }}>
+      <Text style={{ alignSelf: 'center', fontSize: 40, fontWeight: '700' }}>
+        Signs of:
       </Text>
       <View
         style={{
-          borderColor: "red",
-          borderWidth: 1,
-          width: "100%",
+          width: '100%',
+          flexDirection: 'row',
+          paddingHorizontal: 16,
         }}
       >
-        <Text>
-          Blue/Grey Lips Uncouncious person that is unable to be woken up
-          Breathing very slow or not at avaliable Make sure to call 911 if you
-          suspect that it is an overdose
-        </Text>
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            borderBottomColor: 'transparent',
+            height: 160,
+            width: '50%',
+            padding: 5,
+          }}
+        >
+          <Text style={{ fontSize: 12 }}>
+            <Text style={{ fontWeight: 'bold' }}>Overdose:{'\n'}</Text>
+            Blue/Grey Lips Uncouncious person that is unable to be woken up
+            Breathing very slow or not at avaliable Make sure to call 911 if you
+            suspect that it is an overdose
+          </Text>
+        </View>
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            borderBottomColor: 'transparent',
+            height: 160,
+            width: '50%',
+            padding: 5,
+          }}
+        >
+          <Text style={{ fontSize: 12 }}>
+            <Text style={{ fontWeight: 'bold' }}>Substance Abuse:{'\n'}</Text>
+            Desire in which to take opioids regardless of the potential social,
+            economic or health consequences that could occur. An overpowering
+            need that drives the individual to seek out opioids with no care of
+            the risks. Withdrawal occurs when not taking
+          </Text>
+        </View>
       </View>
     </View>
   );
 }
 function Help() {
   return (
-    <View>
-      <Text>Help line: 1-800-662-HELP</Text>
+    <View style={{ margin: 10 }}>
+      <Text>
+        Help line:{' '}
+        <Text
+          style={{ textDecorationLine: 'underline', color: 'blue' }}
+          onPress={() => Linking.openURL('tel:18006624357')}
+        >
+          1-800-662-HELP
+        </Text>
+      </Text>
     </View>
   );
 }
@@ -180,8 +246,8 @@ export default function App() {
           name="NarcanScreen"
           component={NarcanScreen}
           options={{
-            title: "Narcan (Narcan)",
-            headerTitleAlign: "center",
+            title: 'Narcan (Narcan)',
+            headerTitleAlign: 'center',
           }}
         />
       </Stack.Navigator>
@@ -192,9 +258,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
     height: 400,
